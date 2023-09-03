7cc69f3a79a581240a52e5219cf09e48e55af939
ch04937 <97hernandez.c@gmail.com>
2023-09-02 20:03:50 -0500

    todo send response

--- src/routes/calendar/addEvent.js
+++ src/routes/calendar/addEvent.js
@@ -1,4 +1,13 @@
+const { v4 } = require("uuid");
+
 module.exports = (req, res) => {
+  const { appId, userId, role } = req.user;
+  const { date } = req.body;
+  let eventId = v4();
+  if (role === "admin") {
+    let calendar = { calendarId: v4(), adminId: userId };
+    let event = { eventId, date };
+  }
   console.log("req.user", req.user);
   console.log("req.body", req.body);
 };
