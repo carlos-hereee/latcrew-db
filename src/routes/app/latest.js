f0cde79c4c3aa230cc62549ee809fb5179d0248b
ch04937 <97hernandez.c@gmail.com>
2023-09-05 17:42:27 -0500

    not app send 404

--- src/routes/app/latest.js
+++ src/routes/app/latest.js
@@ -1,9 +1,11 @@
 const getApp = require("../../db/models/app/getApp");
+const msg = require("../../data/error.message.json");
 
 module.exports = async (req, res) => {
   const { appId } = req.user;
-  console.log("appId", appId);
+  // console.log("appId", appId);
   const app = await getApp({ appId });
+  if (!app) return res.status(404).json(msg.appNotFound).end();
 
   // if (data.length > 0) {
   console.log("lastest and greatest", app);
